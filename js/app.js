ac6f094dbae687b82b971861f8b3af8a9df0cc66
Maciek Milewski <maciekk.milewski@gmail.com>
2017-01-30 14:16:36 +0100

    Added scroll on button feature

--- js/app.js
+++ js/app.js
@@ -2,33 +2,50 @@ $(document).ready(function() {
 
     //Change Navigation when scrolled
     var newNav = function() {
-            var stillNav = $("#stillNav");
-            var scrolledNav = $("#movedNav");
+        var stillNav = $("#stillNav");
+        var scrolledNav = $("#movedNav");
 
-            $(window).scroll(function() {
-                var scrollPos = $(window).scrollTop();
-                var stillNavPos = stillNav.offset().top;
-
-
-                if (scrollPos > 20) {
-
-                    stillNav.hide(300);
-                    scrolledNav.show(300);
-
-                } else {
-
-                    stillNav.show(300);
-                    scrolledNav.hide(300);
-
-                }
+        $(window).scroll(function() {
+            var scrollPos = $(window).scrollTop();
+            var stillNavPos = stillNav.offset().top;
 
 
-            })
-        }
-        //Arrow down button
+            if (scrollPos > 20) {
 
+                stillNav.hide(300);
+                scrolledNav.show(300);
+
+            } else {
+
+                stillNav.show(300);
+                scrolledNav.hide(300);
+
+            }
+
+
+        })
+    };
+
+    //Arrow down button
     var buttonScroll = function() {
+        var scrollButton = $(".scroll-down-btn");
+        var navHeight = $("#movedNav").height();
+        scrollButton.click(function() {
+            var destination = $("#know-us");
+            $("html, body").animate({
+                scrollTop: ($(destination).offset().top - navHeight)
+            }, 1000);
+        })
 
-    }
+    };
+
+
+
+
+
+
+
+
     newNav();
+    buttonScroll();
 })
